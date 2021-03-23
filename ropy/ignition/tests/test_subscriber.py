c292d6016cecd0a3cfe7fd1eef3aef0318101a5a
FirefoxMetzger <sebastian@wallkoetter.net>
2021-03-23 22:34:25 +0100

    added basic test

---
+++ ropy/ignition/tests/test_subscriber.py
@@ -0,0 +1,19 @@
+import ropy.ignition as ign
+import subprocess
+import time
+import psutil
+
+def test_subscriber():
+    gazebo = subprocess.Popen(
+        ["ign", "gazebo", "shapes.sdf", "-r", "-s"]
+    )
+
+    with ign.Subscriber("/clock") as clock:
+        msg = clock.recv()
+    
+    assert msg is not None
+
+    pobj = psutil.Process(gazebo.pid)
+    for c in pobj.children(recursive=True):
+        c.kill()
+    pobj.kill()
