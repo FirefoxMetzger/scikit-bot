5ad662e136468236740151f2414379dd7f07a2a3
Sebastian Wallkötter <sebastian@wallkoetter.net>
2021-03-24 06:47:46 +0100

    Test with Ignition Support (#2)
    Gets current coverage to 100% 🚀

---
+++ ropy/ignition/tests/test_subscriber.py
@@ -0,0 +1,52 @@
+import ropy.ignition as ign
+import subprocess
+import time
+import psutil
+import pytest
+
+
+@pytest.fixture
+def ign_instance():
+    """Start a gazebo instance to test subscription"""
+
+    gazebo = subprocess.Popen(["ign", "gazebo", "shapes.sdf", "-s"])
+
+    # wait for gazebo to start
+    time.sleep(3)
+
+    yield
+
+    pobj = psutil.Process(gazebo.pid)
+    for c in pobj.children(recursive=True):
+        c.kill()
+    pobj.kill()
+    gazebo.terminate()
+
+
+def test_subscriber_raw(ign_instance):
+    with ign.Subscriber("/clock") as clock:
+        msg = clock.recv()
+
+    assert msg is not None
+
+
+def test_subscriber_parse(ign_instance):
+    def parse_clock(msg):
+        return ign.messages.Clock().parse(msg[2])
+
+    with ign.Subscriber("/clock", parser=parse_clock) as clock:
+        msg = clock.recv()
+
+    assert msg.sim.sec == 0 and msg.sim.nsec == 0
+
+
+def test_subscriber_noblock(ign_instance):
+    with pytest.raises(IOError):
+        with ign.Subscriber("/clock") as clock:
+            msg = clock.recv(blocking=False)
+
+
+def test_subscriber_address_error():
+    with pytest.raises(IOError):
+        with ign.Subscriber("/clock") as clock:
+            msg = clock.recv()
