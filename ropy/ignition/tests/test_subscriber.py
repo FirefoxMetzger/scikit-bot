59165247a5f68d0ef8ea9cd36b8f1e731092d2f4
FirefoxMetzger <sebastian@wallkoetter.net>
2021-03-24 05:19:28 +0100

    blackify

--- ropy/ignition/tests/test_subscriber.py
+++ ropy/ignition/tests/test_subscriber.py
@@ -3,14 +3,13 @@ import subprocess
 import time
 import psutil
 
+
 def test_subscriber():
-    gazebo = subprocess.Popen(
-        ["ign", "gazebo", "shapes.sdf", "-r", "-s"]
-    )
+    gazebo = subprocess.Popen(["ign", "gazebo", "shapes.sdf", "-r", "-s"])
 
     with ign.Subscriber("/clock") as clock:
         msg = clock.recv()
-    
+
     assert msg is not None
 
     pobj = psutil.Process(gazebo.pid)
