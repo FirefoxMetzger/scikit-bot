67b0ce562969d47a621f5b086b45a0589f41011c
FirefoxMetzger <sebastian@wallkoetter.net>
2021-03-25 07:03:30 +0100

    update docs

--- ropy/trajectory/spline.py
+++ ropy/trajectory/spline.py
@@ -6,21 +6,22 @@ def spline(
     t: np.array,
     control_points: np.array,
     *,
-    t_k: np.array = None,
+    t_control: np.array = None,
     degree: int = 3,
     t_min: float = 0,
     t_max: float = 1
 ) -> np.array:
-    """Evaluate the B-spline given by control_points at t.
+    """Evaluate the trajectory given by control_points at t using B-spline
+    interpolation.
 
     ``spline`` constructs a ``degree``-times differentiable trajectory using the
-    given control points and then evaluates the resulting trajectory at position
-    ``t``. It does so using B-splines. By default, control points are spaced out
-    evenly in the interval [t_min, t_max] where ``t=t_min`` results in
+    given control points and then evaluates the resulting trajectory at ``t``.
+    It does so using B-splines. By default, control points are spaced out evenly
+    in the interval ``[t_min, t_max]`` where ``t=t_min`` results in
     ``control_points[0]`` and ``t=t_max`` results in ``control_poins[-1]``.
     Alternatively, the spacing of control points can be set manually by
-    specifying ``t_k``. In this case, the inequality ``t_k[0] <= t_min <= t_max
-    <= t_k[-1]`` must hold.
+    specifying ``t_k``. In this case, the inequality ``t_control[0] <= t_min <=
+    t_max <= t_control[-1]`` must hold.
 
     Parameters
     ----------
@@ -33,7 +34,7 @@ def spline(
         remaining dimensions are used to interpolate between. By default,
         control points are equally spaced within ``[t_min, t_max]`` unless
         ``t_k`` is given.
-    t_k : np.array, None
+    t_control : np.array, None
         A sequence of increasing floats determining the position of the control
         points along the trajectory. None by default, which results in an
         equidistant spacing of points. If set, the following inequality must
@@ -62,10 +63,10 @@ def spline(
     t = np.asarray(t)
     control_points = np.asarray(control_points)
 
-    if t_k is None:
-        t_k = np.linspace(t_min, t_max, len(control_points), dtype=np.float_)
+    if t_control is None:
+        t_control = np.linspace(t_min, t_max, len(control_points), dtype=np.float_)
     else:
-        t_k = np.asarray(t_k)
+        t_control = np.asarray(t_control)
 
-    tck, u = splprep(control_points.T, u=t_k, s=0, ub=t_min, ue=t_max, k=degree)
+    tck, u = splprep(control_points.T, u=t_control, s=0, ub=t_min, ue=t_max, k=degree)
     return np.stack(splev(t, tck, ext=2), axis=0)
