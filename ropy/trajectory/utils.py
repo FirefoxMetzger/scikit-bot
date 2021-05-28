b2f0a31cf995ad11783c06d0f90accc0e6f5f1ac
FirefoxMetzger <sebastian@wallkoetter.net>
2021-05-28 18:11:25 +0200

    FEAT: add cumulative integral

--- ropy/trajectory/utils.py
+++ ropy/trajectory/utils.py
@@ -2,14 +2,12 @@ import numpy as np
 from numpy.typing import ArrayLike
 
 
-def discrete_integral(
-    control_points: ArrayLike, t: ArrayLike = None, *, axis: int = 0
-) -> float:
+def integral(control_points: ArrayLike, t: ArrayLike = None, *, axis: int = 0) -> float:
     """Estimate the integral along a curve.
 
     Estimates the integral of a time-parameterized curve along the chosen axis.
-    The curve is given by a sequence of control points and the respective times
-    when a control point is reached. The time need not be spaced out uniformly.
+    The curve is given by a sequence of control points and their respective
+    times. These times need not be spaced out uniformly.
 
     Parameters
     ----------
@@ -53,3 +51,62 @@ def discrete_integral(
     delta_t = t_upper - t_lower
 
     return np.sum(f_k * delta_t, axis=axis)
+
+
+def cumulative_integral(
+    control_points: ArrayLike, t: ArrayLike = None, *, axis: int = 0
+) -> np.ndarray:
+    """Estimate the cumulative integral along a curve.
+
+    Estimates the cumulative integral of a time-parameterized curve along the
+    chosen axis. The curve is given by a sequence of control points and their
+    respective times. These times need not be spaced out uniformly.
+
+
+    Parameters
+    ----------
+    control_points : ArrayLike
+        The values of the curve at time points t.
+    t : ArrayLike
+        The time points at which the curve was evaluated. If None, it will be
+        set to ``np.linspace(0, 1, control_points.shape[axis])``.
+    axis : int
+        The axis along which to integrate. All other axes are
+        treated as batch dimensions.
+
+    Returns
+    -------
+    estimate : float
+        The estimate of the integral under the curve.
+
+    Notes
+    -----
+    The shapes of control_points and t must match or be broadcastable.
+
+    """
+
+    # This implementation uses the trapezoidal rule
+
+    control_points = np.asarray(control_points)
+
+    if t is None:
+        t = np.linspace(0, 1, control_points.shape[axis])
+    else:
+        t = np.asarray(t)
+
+    f_k = np.zeros_like(control_points)
+
+    f_range = np.arange(control_points.shape[axis] - 1)
+    f_lower = np.take(control_points, f_range, axis=axis)
+    f_upper = np.take(control_points, f_range + 1, axis=axis)
+    f_k = np.zeros_like(control_points)
+    f_k = (f_lower + f_upper) / 2
+    f_k = np.insert(f_k, 0, 0, axis=axis)
+
+    t_range = np.arange(t.shape[axis] - 1)
+    t_lower = np.take(t, t_range, axis=axis)
+    t_upper = np.take(t, t_range + 1, axis=axis)
+    delta_t = t_upper - t_lower
+    delta_t = np.insert(delta_t, 0, 0, axis=axis)
+
+    return np.cumsum(f_k * delta_t, axis=axis)
--- tests/trajectory/test_trajectory_utils.py
+++ tests/trajectory/test_trajectory_utils.py
@@ -9,16 +9,33 @@ import ropy.trajectory as trj
     [
         [np.linspace(0, 5, 200), lambda x: x, 0.5 * 5 ** 2],
         [np.linspace(0, 5, 500), lambda x: x ** 2, 1 / 3 * 5 ** 3],
+        [np.linspace(0, 0, 200), lambda x: x, 0],
     ],
 )
 def test_integration(t, f, expected):
     f_val = f(t)
-    estimate = trj.utils.discrete_integral(f_val, t)
+    estimate = trj.utils.integral(f_val, t)
     assert np.isclose(estimate, expected)
 
 
 def test_integration_none():
     f_val = np.linspace(0, 1, 300) ** 2
-    estimate = trj.utils.discrete_integral(f_val)
+    estimate = trj.utils.integral(f_val)
     expected = 1 / 3
     assert np.isclose(estimate, expected)
+
+
+def test_cumulative_integration():
+    t = np.linspace(0, 5, 200)
+    f = lambda x: x
+    f_val = f(t)
+    estimate = trj.utils.cumulative_integral(f_val, t)
+    expected = 0.5 * t ** 2
+    assert np.allclose(estimate, expected)
+
+
+def test_cumulative_integration_none():
+    f_val = np.linspace(0, 1, 500)
+    estimate = trj.utils.cumulative_integral(f_val)
+    expected = 1 / 2 * np.linspace(0, 1, 500) ** 2
+    assert np.allclose(estimate, expected)
