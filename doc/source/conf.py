96739e1211d5e534d601648c9149c0c5b705044b
Sebastian Wallkötter <sebastian@wallkoetter.net>
2021-06-30 09:59:20 +0200

    BUG: ensure transform + trajectory docs are rendered (#9)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -10,10 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
+# import os
+# import sys
+# from pathlib import Path
 
-sys.path.insert(0, os.path.abspath("..\\..\\ropy"))
+# sys.path.insert(0, Path(__file__).parents[2])
 
 
 # -- Project information -----------------------------------------------------
