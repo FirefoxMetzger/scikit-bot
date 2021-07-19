3c087fd63273b115eaf618f0c36ad2fe7e1bb93f
FirefoxMetzger <sebastian@wallkoetter.net>
2021-07-19 12:29:55 +0200

    MAINT: remove test of obsolete function

--- tests/transform/test_projections.py
+++ tests/transform/test_projections.py
@@ -30,21 +30,6 @@ def test_frustum_project(point_in, fov, im_shape, point_out):
 
 
 @pytest.mark.parametrize(
-    "point_in, fov, im_shape, point_out",
-    [
-        ((1, 0, 0), np.pi / 3, (240, 320), (160, 120)),
-        ((3.5, 0, 0), np.pi / 2, (240, 320), (160, 120)),
-        ((1, 0, 0), 1.5 * np.pi, (480, 640), (320, 240)),
-        ((1, 1, 480 / 640), 1.5 * np.pi, (480, 640), (640, 480)),
-        ((1, -1, -480 / 640), 1.5 * np.pi, (480, 640), (0, 0)),
-    ],
-)
-def test_perspective_transform(point_in, fov, im_shape, point_out):
-    proj = rtf.projections.PerspectiveProjection(fov, im_shape)
-    assert np.allclose(proj.transform(point_in), point_out)
-
-
-@pytest.mark.parametrize(
     "parent_coords, child_coords, direction, amount",
     [
         ((1, 1, 0), 1, (1, 0, 0), (0, 1, 0)),
@@ -53,6 +38,6 @@ def test_perspective_transform(point_in, fov, im_shape, point_out):
     ],
 )
 def test_1d_projections(parent_coords, child_coords, direction, amount):
-    proj = rtf.projections.NDPerspectiveProjection(direction, amount)
+    proj = rtf.projections.PerspectiveProjection(direction, amount)
     result = proj.transform(parent_coords)
     assert np.allclose(result, child_coords)
