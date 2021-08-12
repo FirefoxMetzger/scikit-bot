a29ce0cbc25507f989ed7b5db288e612a845605c
FirefoxMetzger <sebastian@wallkoetter.net>
2021-08-12 20:22:31 +0200

    TEST: add panda SDF

--- tests/ignition/test_transform_factory.py
+++ tests/ignition/test_transform_factory.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+from pathlib import Path
 
 import skbot.ignition as ign
 
@@ -8,3 +9,10 @@ def test_sdformat_transform_factory(valid_sdf_string):
     if ign.sdformat.get_version(valid_sdf_string) != "1.8":
         pytest.skip("Wrong Version")
     ign.transform_graph_from_sdf(valid_sdf_string)
+
+
+def test_panda():
+    model_file = Path(__file__).parent / "sdf" / "robots" / "panda" / "model.sdf"
+    sdf_string = model_file.read_text()
+
+    root_frame = ign.transform_graph_from_sdf(sdf_string)
