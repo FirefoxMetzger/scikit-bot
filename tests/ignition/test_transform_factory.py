d3b5acd8adf3cd6e65f176c8c0030d68fbd78e9a
FirefoxMetzger <sebastian@wallkoetter.net>
2021-08-12 19:54:46 +0200

    TEST: rename test file

--- tests/ignition/test_transform_factory.py
+++ tests/ignition/test_transform_factory.py
@@ -4,7 +4,7 @@ import numpy as np
 import skbot.ignition as ign
 
 
-def test_light(valid_sdf_string):
+def test_sdformat_transform_factory(valid_sdf_string):
     if ign.sdformat.get_version(valid_sdf_string) != "1.8":
         pytest.skip("Wrong Version")
-    ign.create_frame_graph(valid_sdf_string)
+    ign.transform_graph_from_sdf(valid_sdf_string)
