d0cabec04375c8675f760536a67aa3e8639f6a70
FirefoxMetzger <sebastian@wallkoetter.net>
2021-09-17 12:38:05 +0200

    FEAT: add generic SDF element

--- skbot/ignition/sdformat/_transform_factory/generic.py
+++ skbot/ignition/sdformat/_transform_factory/generic.py
@@ -10,10 +10,16 @@ objects are then used to construct the actual frame graph.
 """
 
 
-from typing import List, Tuple
+from typing import List, Tuple, Union
 import numpy as np
 from itertools import chain
-from ..sdformat import ParseError
+from ..exceptions import ParseError
+import warnings
+
+
+class Version:
+    def __init__(self, *, sdf_version: str = "1.8"):
+        self.sdf_version = sdf_version
 
 
 class Pose:
@@ -313,7 +319,16 @@ class Model(NamedPoseBearing):
                 frame.attached_to = frame.attached_to + "::__model__"
 
 
+class Actor(NamedPoseBearing):
+    pass
+
+
 class World:
+    """ A simulation World
+    
+    
+    """
+
     def __init__(
         self,
         *,
@@ -407,3 +422,180 @@ class World:
             def __init__(self, *, radius: float = 1, length: float = 1) -> None:
                 self.radius = radius
                 self.length = length
+
+
+class Sdf(Version):
+    """SDFormat Base Element
+
+    This element is a container for multiple simulation worlds (can be one) or
+    for a single fragment of a world (Model, Actor, Light).
+
+    Parameters
+    ----------
+    payload : Union[List[World], Model, Light, Actor]
+        The element contained in this SDF. This can be one :class:`Model`, one :class`Actor`, one
+        :class:`Light`, or a list of :class`Worlds`.
+    version : str
+        The SDFormat version.
+    worlds : List[World]
+        .. depreciated:: SDFormat v1.8
+            Worlds, models, lights, and/or actors can no longer be combined. Use
+            `payload` instead.
+        The worlds contained in this SDF.
+    models : List[Model]
+        .. depreciated:: SDFormat v1.8
+            Starting with SDFormat v1.8 only a single model is supported. Use the
+            `model` kwarg instead.
+
+        The models contained in this SDF.
+    lights : List[Light]
+        .. depreciated:: SDFormat v1.8
+            Starting with SDFormat v1.8 only a single light is supported. Use the
+            `light` kwarg instead.
+
+        The lights contained in this SDF.
+    actors : List[Actor]
+        .. depreciated:: SDFormat v1.8
+            Starting with SDFormat v1.8 only a single actor is supported. Use the
+            `actor` kwarg instead.
+
+        The actors contained in this SDF.
+
+    Attributes
+    ----------
+    worlds : List[World]
+        The worlds contained in the SDF file.
+    model: Model
+        The model contained in the SDF file.
+    light: Light
+        The light contained in the SDF file.
+    actor: Actor
+        The actor contained in the SDF file.
+    version : str
+        The SDFormat version.
+    models : List[Model]
+        .. depreciated:: SDFormat v1.8
+            Use the `Sdf.model` instead.
+    lights : List[Light]
+        .. depreciated:: SDFormat v1.8
+            Use the `Sdf.light` instead.
+    actors : List[Actor]
+        .. depreciated:: SDFormat v1.8
+            Use the `Sdf.actor` instead.
+
+    """
+
+    def __init__(
+        self,
+        *,
+        payload: Union[List[World], Model, Light, Actor] = None,
+        version: str = "1.8",
+        worlds: List[World] = None,
+        models: List[Model] = None,
+        lights: List[Light] = None,
+        actors: List[Actor] = None,
+    ) -> None:
+        super().__init__(sdf_version=version)
+        self.version = version
+        self._worlds = []
+        self._actors = []
+        self._models = []
+        self._lights = []
+
+        if self.sdf_version == "1.8":
+            if worlds is not None:
+                raise ValueError(
+                    "`Sdf` does not support the `worlds` kwarg for SDFormat v1.8. Use `payload` instead."
+                )
+            if actors is not None:
+                raise ParseError("`Sdf` only supports a single actor in SDFormat v1.8.")
+            if models is not None:
+                raise ParseError("`Sdf` only supports a single model in SDFormat v1.8.")
+            if lights is not None:
+                raise ParseError("`Sdf` only supports a single light in SDFormat v1.8.")
+
+            if isinstance(payload, list) and all(
+                [isinstance(x, World) for x in payload]
+            ):
+                self._worlds = payload
+            elif isinstance(payload, Actor):
+                self._actors.append(payload)
+            elif isinstance(payload, Model):
+                self._models.append(payload)
+            elif isinstance(payload, Light):
+                self._lights.append(payload)
+            else:
+                raise ParseError("Invalid `Sdf` element.")
+        elif version in ["1.7", "1.6", "1.5", "1.4", "1.3"]:
+            if payload is not None:
+                raise ParseError(
+                    "`Sdf` does not support `payload` prior to SDFormat v1.8."
+                )
+            if worlds is None:
+                raise ValueError("`Sdf` must specify `worlds` prior to SDFormat v1.8.")
+            if actors is None:
+                raise ValueError("`Sdf` must specify `actors` prior to SDFormat v1.8.")
+            if models is None:
+                raise ValueError("`Sdf` must specify `models` prior to SDFormat v1.8.")
+            if lights is None:
+                raise ValueError("`Sdf` must specify `lights` prior to SDFormat v1.8.")
+
+            self._worlds = worlds
+            self._models = models
+            self._lights = lights
+            self._actors = actors
+        else:
+            raise ParseError("`Sdf` does not exist prior to SDFormat v1.3.")
+
+    @property
+    def worlds(self) -> List[World]:
+        if len(self._worlds) == 0:
+            raise AttributeError("`Sdf` does not contain any worlds.")
+
+        return self._worlds
+
+    @property
+    def actor(self) -> Actor:
+        try:
+            return self._actors[0]
+        except IndexError:
+            raise AttributeError("`Sdf` does not contain an actor.") from None
+
+    @property
+    def model(self) -> Model:
+        try:
+            return self._models[0]
+        except IndexError:
+            raise AttributeError("`Sdf` does not contain a model.") from None
+
+    @property
+    def light(self) -> Light:
+        try:
+            return self._lights[0]
+        except IndexError:
+            raise AttributeError("`Sdf` does not contain a light.") from None
+
+    # Depreciated properties
+    @property
+    def actors(self) -> List[Actor]:
+        warnings.warn(
+            "`sdf.actors` is depreciated since SDFormat v1.8. Use `sdf.actor` instead.",
+            DeprecationWarning,
+        )
+        return self._actors
+
+    @property
+    def models(self) -> List[Model]:
+        warnings.warn(
+            "`sdf.models` is depreciated since SDFormat v1.8. Use `sdf.models` instead.",
+            DeprecationWarning,
+        )
+        return self._models
+
+    @property
+    def lights(self) -> List[Light]:
+        warnings.warn(
+            "`sdf.lights` is depreciated since SDFormat v1.8. Use `sdf.lights` instead.",
+            DeprecationWarning,
+        )
+        return self._lights
