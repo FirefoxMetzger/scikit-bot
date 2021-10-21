7f3063d394568afac6da9981ce47e799ebbb9188
FirefoxMetzger <sebastian@wallkoetter.net>
2021-10-21 08:32:41 +0200

    TEST(ik): test legacy syntax for ccd

--- tests/inverse_kinematics/test_ccd.py
+++ tests/inverse_kinematics/test_ccd.py
@@ -117,7 +117,57 @@ def test_pendulum(double_pendulum):
     final_pos = tool_frame.transform((0, 0, 0), base_frame)
 
     assert np.allclose(final_pos, root_pos, atol=0.001)
-    # assert np.allclose(angles, expected)
+
+
+def test_pendulum_legacy(double_pendulum):
+    base_frame: tf.Frame
+    joints: List[joint_types]
+    base_frame, joints = double_pendulum
+    tool_frame = base_frame.find_frame(".../lower_link")
+
+    for joint in joints:
+        joint.param = (joint.upper_limit + joint.lower_limit) / 4
+
+    root_pos = tool_frame.transform((0, 0, 0), base_frame)
+    expected = np.zeros(len(joints))
+
+    for idx, joint in enumerate(joints):
+        expected[idx] = joint.param
+        joint.param = (joint.upper_limit + joint.lower_limit) / 2
+
+    angles = ik.ccd((0, 0, 0), root_pos, tool_frame, base_frame, joints)
+    final_pos = tool_frame.transform((0, 0, 0), base_frame)
+
+    assert np.allclose(final_pos, root_pos, atol=0.001)
+
+
+def test_pendulum_legacy_kwargs(double_pendulum):
+    base_frame: tf.Frame
+    joints: List[joint_types]
+    base_frame, joints = double_pendulum
+    tool_frame = base_frame.find_frame(".../lower_link")
+
+    for joint in joints:
+        joint.param = (joint.upper_limit + joint.lower_limit) / 4
+
+    root_pos = tool_frame.transform((0, 0, 0), base_frame)
+    expected = np.zeros(len(joints))
+
+    for idx, joint in enumerate(joints):
+        expected[idx] = joint.param
+        joint.param = (joint.upper_limit + joint.lower_limit) / 2
+
+    angles = ik.ccd(
+        [],
+        pointA=(0, 0, 0),
+        pointB=root_pos,
+        frameA=tool_frame,
+        frameB=base_frame,
+        cycle_links=joints,
+    )
+    final_pos = tool_frame.transform((0, 0, 0), base_frame)
+
+    assert np.allclose(final_pos, root_pos, atol=0.001)
 
 
 def test_ccd_maxiter(double_pendulum):
