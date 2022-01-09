232383d8dea036ae50a7360a00aafdddffa4bc15
FirefoxMetzger <sebastian@wallkoetter.net>
2022-01-09 07:22:42 +0100

    MAINT: organize sdformat bindings

--- skbot/ignition/sdformat/generic_sdf/shapes.py
+++ skbot/ignition/sdformat/generic_sdf/shapes.py
@@ -55,3 +55,9 @@ class Box(ElementBase):
     def __init__(self, *, sdf_version: str) -> None:
         warnings.warn("`Box` has not been implemented yet.")
         super().__init__(sdf_version=sdf_version)
+
+
+class Sphere(ElementBase):
+    def __init__(self, *, sdf_version: str) -> None:
+        warnings.warn("`Sphere` has not been implemented yet.")
+        super().__init__(sdf_version=sdf_version)
